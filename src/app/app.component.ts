fea6f3bcd29af43392f7d67d3d2ea33180d770ce
Missael Padilla <lmpadillar@gmail.com>
2024-01-16 20:08:16 -0800

    Complete Challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'order-summary-component';
+
+  displayCard = true;
+  isModalOpen = false;
+
+  plan: string = 'Annual Plan';
+  price: string = '$59.99/year';
+
+  viewCard() {
+    this.displayCard = true;
+  }
+
+  closeCard() {
+    this.displayCard = false;
+  }
+
+  
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  setPlan(plan:string, price: string) {
+    this.plan = plan;
+    this.price = price;
+    this.closeModal();
+  }
 }
